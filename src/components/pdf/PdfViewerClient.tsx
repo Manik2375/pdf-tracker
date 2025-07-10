a4771a3a9b99d1cae70e72ff83df86d80781a0e8
Manik2375 <manikkpr2375@gmail.com>
2025-07-10 22:46:20 +0530

    feat: support for whole pdf

--- src/components/pdf/PdfViewerClient.tsx
+++ src/components/pdf/PdfViewerClient.tsx
@@ -1,6 +1,7 @@
 "use client";
 import dynamic from "next/dynamic";
-import { ComponentType } from "react";
+import "react-pdf/dist/Page/TextLayer.css";
+import { ComponentType, useState } from "react";
 
 interface PdfViewerClientProps {
   pdfLink: string;
@@ -10,23 +11,35 @@ const DynamicPdfViewer = dynamic(async () => {
   const { Document, Page, pdfjs } = await import("react-pdf");
   pdfjs.GlobalWorkerOptions.workerSrc = "/pdf.worker.mjs";
 
-  const PdfComponent: ComponentType<PdfViewerClientProps> = ({ pdfLink }) => (
-    <div>
-      <Document
-        file={pdfLink}
-        onLoadSuccess={() => {
-          console.log("pdf loaded");
-        }}
-      >
-        <Page
-          pageNumber={15}
-          renderTextLayer={false}
-          renderAnnotationLayer={false}
-          className="shadow rounded mx-auto"
-        />
-      </Document>
-    </div>
-  );
+  const PdfComponent: ComponentType<PdfViewerClientProps> = ({ pdfLink }) => {
+    const [numPages, setNumPages] = useState<number>(0);
+    const [outline, setOutline] = useState({});
+    return (
+      <div className="overflow-y-scroll h-[80vh] px-4 py-6 flex flex-col items-center space-y-6">
+        <Document
+          file={"/test.pdf"}
+          onLoadSuccess={async (pdf) => {
+            console.log(pdf.numPages);
+            setNumPages(pdf.numPages);
+            const out = await pdf.getOutline();
+            setOutline(out);
+          }}
+        >
+          {Array.from(Array(numPages)).map((_, i) => {
+            return (
+              <Page
+                key={i}
+                pageNumber={i + 1}
+                renderTextLayer={true}
+                renderAnnotationLayer={false}
+                className="shadow rounded mx-auto"
+              />
+            );
+          })}
+        </Document>
+      </div>
+    );
+  };
   return PdfComponent;
 });
 
