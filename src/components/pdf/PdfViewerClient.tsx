44e45917fa3a5b9f620b5d8f37c13ceee51fdfaf
Manik2375 <manikkpr2375@gmail.com>
2025-07-12 16:29:50 +0530

    feat: ability to zoom in and out

--- src/components/pdf/PdfViewerClient.tsx
+++ src/components/pdf/PdfViewerClient.tsx
@@ -14,26 +14,12 @@ const DynamicPdfViewer = dynamic(async () => {
   const PdfComponent: ComponentType<PdfViewerClientProps> = ({ pdfLink }) => {
     const [currentPage, setCurrentPage] = useState<number>(1);
     const [numPages, setNumPages] = useState<number>(0);
+    const [scale, setScale] = useState<number>(1.0);
     const [fullscreen, setFullscreen] = useState<boolean>(false);
     const [outline, setOutline] = useState({});
     const pageRefs = useRef<HTMLDivElement[] | null>([]);
 
     const containerRef = useRef<HTMLDivElement | null>(null);
-
-    const changePageNumber = (pageNum: number) => {
-      const page = pageRefs.current?.find((page) => {
-        if (page.dataset.pagenumber) {
-          return pageNum === +page.dataset.pagenumber;
-        }
-      });
-      setCurrentPage(pageNum);
-      page?.scrollIntoView();
-    };
-
-    const handleNavButton = (action: "prev" | "next") => {
-      const newPage = action === "prev" ? currentPage - 1 : currentPage + 1;
-      changePageNumber(newPage);
-    };
 
     useEffect(() => {
       const handleFullscreenChange = () => {
@@ -49,6 +35,44 @@ const DynamicPdfViewer = dynamic(async () => {
       };
     }, []);
 
+    useEffect(() => {
+      const handleWheel = (e: WheelEvent) => {
+        if (e.ctrlKey || e.metaKey) {
+          e.preventDefault();
+
+          setScale((prev) => {
+            const newState = e.deltaY > 0 ? prev - 0.1 : prev + 0.1;
+
+            return Math.min(3, Math.max(0.5, newState));
+          });
+        }
+      };
+      window.addEventListener("wheel", handleWheel, { passive: false });
+
+      return () => {
+        window.removeEventListener("wheel", handleWheel);
+      };
+    }, []);
+
+    const changePageNumber = (pageNum: number) => {
+      const page = pageRefs.current?.find((page) => {
+        if (page.dataset.pagenumber) {
+          return pageNum === +page.dataset.pagenumber;
+        }
+      });
+      let newPageNum = pageNum;
+      if (pageNum <= 0) newPageNum = 1;
+      else if (pageNum > numPages) newPageNum = numPages;
+      setCurrentPage(newPageNum);
+      page?.scrollIntoView();
+    };
+
+    const handleNavButton = (action: "prev" | "next") => {
+      const newPage = action === "prev" ? currentPage - 1 : currentPage + 1;
+      changePageNumber(newPage);
+    };
+
+
     const handleFullScreen = () => {
       const container = containerRef.current;
       if (!container) return;
@@ -63,79 +87,124 @@ const DynamicPdfViewer = dynamic(async () => {
     };
     return (
       <div
-        className={`${fullscreen ? "" : "px-5 rounded-box"} relative flex py-6 pt-0 space-y-6 bg-base-200`}
+        className={`${fullscreen ? "" : "px-5 py-6 rounded-box"} relative flex  pt-0 space-y-6 bg-base-200`}
         ref={containerRef}
       >
         <div
-          className={`overflow-y-auto w-full flex flex-col items-center ${fullscreen ? "h-full" : " mt-20"}`}
+          className={`overflow-y-auto w-full flex flex-col items-center ${fullscreen ? "h-full" : " mt-20 h-[85vh]"}`}
         >
           <div
             className={`${fullscreen ? "sticky" : "absolute rounded-t-box"} flex justify-between items-center top-0 left-0 right-0 p-5 w-full bg-[#0000004a] backdrop-blur-xs z-10`}
           >
             <p>Chronicles of owl</p>
-            <div className="flex gap-2">
-              <button
-                className="btn btn-neutral aspect-square w-10 p-0"
-                onClick={() => {
-                  handleNavButton("prev");
-                }}
-              >
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
+            <div className="flex gap-5 items-center">
+              <div className="flex gap-2">
+                <button
+                  className="btn btn-neutral aspect-square w-10 p-0"
+                  onClick={() => {
+                    handleNavButton("prev");
+                  }}
                 >
-                  <path
-                    d="M15 6L9 12L15 18"
-                    className="stroke-neutral-content"
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M15 6L9 12L15 18"
+                      className="stroke-neutral-content"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </button>
+                <input
+                  type="number"
+                  value={currentPage}
+                  min={1}
+                  max={numPages}
+                  onChange={(e) => {
+                    const pageNum = Math.min(
+                      numPages,
+                      Math.max(1, parseInt(e.currentTarget.value) || 1),
+                    );
+                    changePageNumber(pageNum);
+                  }}
+                  className="input w-20 text-center"
+                />
+                <button
+                  className="btn btn-neutral aspect-square w-10 p-0"
+                  onClick={() => {
+                    handleNavButton("next");
+                  }}
+                >
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M9 6L15 12L9 18"
+                      className="stroke-neutral-content"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <div className="flex gap-2">
+                <button
+                  className="btn btn-ghost btn-neutral  p-1 rounded-full aspect-square"
+                  onClick={() => setScale((prev) => Math.max(0.5, prev - 0.1))}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
                     strokeWidth="2"
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                  />
-                </svg>
-              </button>
-              <input
-                type="number"
-                value={currentPage}
-                min={1}
-                max={numPages}
-                onChange={(e) => {
-                  const pageNum = Math.min(
-                    numPages,
-                    Math.max(1, parseInt(e.currentTarget.value) || 1),
-                  );
-                  changePageNumber(pageNum);
-                }}
-                className="input w-20 text-center"
-              />
-              <button
-                className="btn btn-neutral aspect-square w-10 p-0"
-                onClick={() => {
-                  handleNavButton("next");
-                }}
-              >
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <circle cx="11" cy="11" r="8" />
+                    <line x1="8" y1="11" x2="14" y2="11" />
+                    <line x1="21" y1="21" x2="16.65" y2="16.65" />
+                  </svg>
+                </button>
+                <button
+                  className="btn btn-ghost btn-neutral p-1 rounded-full aspect-square"
+                  onClick={() => setScale((prev) => Math.min(2.0, prev + 0.1))}
                 >
-                  <path
-                    d="M9 6L15 12L9 18"
-                    className="stroke-neutral-content"
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
                     strokeWidth="2"
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                  />
-                </svg>
-              </button>
+                  >
+                    <circle cx="11" cy="11" r="8" />
+                    <line x1="11" y1="8" x2="11" y2="14" />
+                    <line x1="8" y1="11" x2="14" y2="11" />
+                    <line x1="21" y1="21" x2="16.65" y2="16.65" />
+                  </svg>
+                </button>
+              </div>
             </div>
             <div>
               <button
-                className="btn text-neutral-content btn-ghost btn-neutral"
+                className="btn btn-ghost btn-neutral"
                 onClick={handleFullScreen}
               >
                 <svg
@@ -178,9 +247,14 @@ const DynamicPdfViewer = dynamic(async () => {
                   ref={(el) => {
                     if (pageRefs.current && el) pageRefs.current[i] = el;
                   }}
+                  style={{
+                    minHeight: `calc(80vh * ${scale})`,
+                    width: "100%",
+                  }}
                 >
                   <Page
                     pageNumber={i + 1}
+                    scale={scale}
                     renderTextLayer={true}
                     renderAnnotationLayer={false}
                     className="shadow rounded mx-auto"
