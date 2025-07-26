04421e2f9b7693aae12e40905063f0b462ac4f68
Manik2375 <manikkpr2375@gmail.com>
2025-07-26 18:10:51 +0530

    fix: auth.js middleware getToken issue - https://stackoverflow.com/questions/71363829/next-auth-js-i-cant-get-token-with-gettokenreq

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,22 +3,22 @@ import { getToken } from "next-auth/jwt";
 
 export async function middleware(request: NextRequest) {
   const token = await getToken({
+    cookieName:
+      process.env.VERCEL_ENV === "development"
+        ? "authjs.session-token"
+        : "__Secure-authjs.session-token",
     req: request,
     secret: process.env.AUTH_SECRET,
   });
-  console.log("Middleware working")
+  // console.log("Middleware working", token);
   const url = request.nextUrl;
   if (token && url.pathname === "/") {
     return NextResponse.redirect(new URL("/home", request.url));
   } else if (!token && url.pathname !== "/") {
-     return NextResponse.redirect(new URL("/", request.url));
+    return NextResponse.redirect(new URL("/", request.url));
   }
 }
 
 export const config = {
-  matcher: [
-    "/app/:path*",
-    "/",
-    "/home/:path*"
-  ],
+  matcher: ["/app/:path*", "/", "/home/:path*"],
 };
